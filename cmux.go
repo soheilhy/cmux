soheilhy/cmux

commit b90740dfa9b286c06f58fa798929e2d2cb1299e5

Fix send on closed channel
If a matcher go-routine mateches a connection after the root
listener was closed, cmux will panic since all connection channels
will be closed. This commit avoids closing connection channels but
signals the matcher go-routines that the listener is closed via
a done channel.

Tested: Added a test case for this issue.

cmux.go
@@ -80,7 +80,8 @@ type cMux struct {
 func (m *cMux) Match(matchers ...Matcher) (l net.Listener) {
 	ml := muxListener{
 		Listener: m.root,
-		cch:      make(chan net.Conn, m.bufLen),
+		connc:    make(chan net.Conn, m.bufLen),
+		donec:    make(chan struct{}),
 	}
 	m.sls = append(m.sls, matchersListener{ss: matchers, l: ml})
 	return ml
@@ -89,7 +90,7 @@ func (m *cMux) Match(matchers ...Matcher) (l net.Listener) {
 func (m *cMux) Serve() error {
 	defer func() {
 		for _, sl := range m.sls {
-			close(sl.l.cch)
+			close(sl.l.donec)
 		}
 	}()
 
@@ -109,14 +110,18 @@ func (m *cMux) Serve() error {
 func (m *cMux) serve(c net.Conn) {
 	muc := newMuxConn(c)
 	matched := false
-outer:
 	for _, sl := range m.sls {
 		for _, s := range sl.ss {
 			matched = s(muc.sniffer())
 			muc.reset()
 			if matched {
-				sl.l.cch <- muc
-				break outer
+				select {
+				// TODO(soheil): threre is a possiblity of having unclosed connection.
+				case sl.l.connc <- muc:
+				case <-sl.l.donec:
+					c.Close()
+				}
+				return
 			}
 		}
 	}
@@ -148,11 +153,12 @@ func (m *cMux) handleErr(err error) bool {
 
 type muxListener struct {
 	net.Listener
-	cch chan net.Conn
+	connc chan net.Conn
+	donec chan struct{}
 }
 
 func (l muxListener) Accept() (c net.Conn, err error) {
-	c, ok := <-l.cch
+	c, ok := <-l.connc
 	if !ok {
 		return nil, ErrListenerClosed
 	}

cmux_test.go
@@ -168,3 +168,16 @@ func TestErrorHandler(t *testing.T) {
 		t.Error("rpc got a response")
 	}
 }
+
+type closerConn struct {
+	net.Conn
+}
+
+func (c closerConn) Close() error { return nil }
+
+func TestClosed(t *testing.T) {
+	mux := &cMux{}
+	lis := mux.Match(Any()).(muxListener)
+	close(lis.donec)
+	mux.serve(closerConn{})
+}

commit 89dd8ce1fdc0f27d316ffe66afe32d0446264b40

Return an error in Accept() when root is closed.
As reported in #4, when the root listener is closed, calling Accept
on mux'd listeners would block forever, instead of returning an error.
This is because of a bug introduced in b90740d.

This commit fixes #4 by selecting on both donec and connc of muxed
listeners.

Added a test case to guard against this issue.

cmux.go
@@ -158,11 +158,12 @@ type muxListener struct {
 }
 
 func (l muxListener) Accept() (c net.Conn, err error) {
-	c, ok := <-l.connc
-	if !ok {
+	select {
+	case c = <-l.connc:
+		return c, nil
+	case <-l.donec:
 		return nil, ErrListenerClosed
 	}
-	return c, nil
 }
 
 type MuxConn struct {

cmux_test.go
@@ -180,4 +180,8 @@ func TestClosed(t *testing.T) {
 	lis := mux.Match(Any()).(muxListener)
 	close(lis.donec)
 	mux.serve(closerConn{})
+	_, err := lis.Accept()
+	if _, ok := err.(errListenerClosed); !ok {
+		t.Errorf("expected errListenerClosed got %v", err)
+	}
 }