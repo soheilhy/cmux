b250311b695112d7e94b9861efb39374aba14739
Cassondra Foesch <puellanivis@gmail.com>
2016-09-26 04:15:33 +0000

    fix up buffer.go some more, and actually make it work, instead of crash on test

--- buffer.go
+++ buffer.go
@@ -20,55 +20,45 @@ import (
 )
 
 // bufferedReader is an optimized implementation of io.Reader that behaves like
-//	io.MultiReader(bytes.NewReader(buffer.Bytes()), io.TeeReader(source, buffer))
-// Except it does not remove the bytes.Reader from the MultiReader when it hits EOF.
+// ```
+// io.MultiReader(bytes.NewReader(buffer.Bytes()), io.TeeReader(source, buffer))
+// ```
+// without allocating.
 type bufferedReader struct {
-	buffer    bytes.Buffer
-	snif, src io.Reader
-	lastErr   error
+	source  io.Reader
+	src     io.Reader
+
+	buf	bytes.Buffer
+
+	rbuf	[]byte
+	off     int
 }
 
-func newBufferedReader(r io.Reader, snif bool) *bufferedReader {
-	s := &bufferedReader{
-		src: r,
-	}
-	
-	s.reset(snif)
-	
-	return s
-}
+func (s *bufferedReader) Read(p []byte) (int, error) {
+	if s.off < len(s.rbuf) {
+		n := copy(p, s.rbuf[s.off:])
+		s.off += n
+		return n, nil
 
-func (s *bufferedReader) Read(p []byte) (n int, err error) {
-	if s.buffer.Len() > 0 {
-		// If we have already read something from the buffer before, we return the
-		// same data and the last error if any. We need to immediately return,
-		// otherwise we may block for ever, if we try to be smart and call
-		// source.Read() seeking a little bit of more data.
-		
-		// (puellanivis) This behavior seems really weird, why is read data being
-		// returned twice, and not separate readers?
-		n, _ = s.buffer.Read(p)
-		
-	} else {
-		// If there is nothing more to return in the sniffed buffer, read from the
-		// source.
-		n, s.lastErr = s.snif.Read(p)
+	} else if s.src == s.source {
+		s.rbuf = nil
+		s.buf.Reset()
 	}
 
-	return n, s.lastErr
+	return s.src.Read(p)
 }
 
-func (s *bufferedReader) Reset(snif bool) {
-	s.buffer.Reset()
+func (s *bufferedReader) reset(snif bool) {
+	s.off = 0
+	s.rbuf = s.buf.Bytes()
 
 	if !snif {
-		s.snif = s.src
+		s.src = s.source
+		s.buf.Reset()
+		return
 	}
 
-	if s.snif == s.src {
-		// this is a just in case.
-		// if a bufferedReader should never transition from snif==false to snif==true,
-		// then this whole if clause can just be removed.
-		s.snif = io.TeeReader(s.src, &s.buffer)
+	if s.src == nil {
+		s.src = io.TeeReader(s.source, &s.buf)
 	}
 }
